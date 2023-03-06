5df68b1486e25969cb94371e285a35f403e6c50e
souzaitor <heitor.souza@vonbraunlabs.com.br>
2023-03-06 19:59:44 -0300

    feat: Implementação bate papo

--- screens/GuiaScreen.js
+++ screens/GuiaScreen.js
@@ -2,25 +2,13 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import {Surface} from 'react-native-paper';
 
-import { SafeAreaView } from 'react-native';
-import { Appbar } from 'react-native-paper';
-
-
 export default function GuiaScreen({ navigation }) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Appbar.Header>
-        <Appbar.Action
-          icon="menu"
-          onPress={() => navigation.openDrawer()}
-        />
-        <Appbar.Content title="Guia" />
-      </Appbar.Header>
       <Surface style={{ flex: 1, backgroundColor: '#121212' }}>
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>Esta é a tela de Guia!</Text>
         </View>
       </Surface>
-    </SafeAreaView>
   );
 }
--- screens/PapoScreen.js
+++ screens/PapoScreen.js
@@ -1,10 +1,125 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { View, Text } from 'react-native';
+import { Surface, Appbar, Provider as PaperProvider, MD3DarkTheme as DefaultTheme, Button } from 'react-native-paper';
 
-export default function PapoScreen() {
+function Question({ question }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Esta é a tela de Bate-Papo!</Text>
+    <View style={{ marginTop: 20 }}>
+      <Text style={{ color: '#fff', fontSize: 20 }}>{question}</Text>
     </View>
+  );
+}
+
+export default function PapoScreen({ navigation }) {
+  const [question, setQuestion] = useState('Gostaria de iniciar o Bate Papo?');
+  const [answer, setAnswer] = useState('');
+  const [positiveResponses, setPositiveResponses] = useState(0);
+
+  const handleAnswer = (response) => {
+    if (question === 'Gostaria de iniciar o Bate Papo?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem sentido ansiedade ou medo excessivo ultimamente?');
+      } else if (response === 'Não') {
+        setQuestion('Tudo bem, talvez outra hora!');
+      }
+    } else if (question === 'Você tem sentido ansiedade ou medo excessivo ultimamente?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem tido pensamentos repetitivos ou obsesivos?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem tido pensamentos repetitivos ou obsesivos?');
+      }
+    } else if (question === 'Você tem tido pensamentos repetitivos ou obsesivos?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem experimentado mudanças significativas no seu humor?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem experimentado mudanças significativas no seu humor?');
+      }
+    } else if (question === 'Você tem experimentado mudanças significativas no seu humor?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem experimentado mudanças significativas no seu padrão de sono?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem experimentado mudanças significativas no seu padrão de sono?');
+      }
+    } else if (question === 'Você tem experimentado mudanças significativas no seu padrão de sono?') {
+        if (response === 'Sim') {
+          setQuestion('Você tem tido dificuldade em realizar suas atividades diárias?');
+          setPositiveResponses(positiveResponses + 1);
+        } else if (response === 'Não') {
+          setQuestion('Você tem tido dificuldade em realizar suas atividades diárias?');
+        }
+    } else if (question === 'Você tem tido dificuldade em realizar suas atividades diárias?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem tido dificuldade em se relacionar com outras pessoas?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem tido dificuldade em se relacionar com outras pessoas?');
+      }
+    } else if (question === 'Você tem tido dificuldade em se relacionar com outras pessoas?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem experimentado alucinações ou delírios?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem experimentado alucinações ou delírios?');
+      }
+    } else if (question === 'Você tem experimentado alucinações ou delírios?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem tido dificuldade em concentrar-se ou em lembrar coisas?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem tido dificuldade em concentrar-se ou em lembrar coisas?');
+      }
+    } else if (question === 'Você tem tido dificuldade em concentrar-se ou em lembrar coisas?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem sentido uma falta de motivação ou interesse em coisas que costumava gostar?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem sentido uma falta de motivação ou interesse em coisas que costumava gostar?');
+      }
+    } else if (question === 'Você tem sentido uma falta de motivação ou interesse em coisas que costumava gostar?') {
+      if (response === 'Sim') {
+        setQuestion('Você tem tido pensamentos ou comportamentos suicidas?');
+        setPositiveResponses(positiveResponses + 1);
+      } else if (response === 'Não') {
+        setQuestion('Você tem tido pensamentos ou comportamentos suicidas?');
+      }
+    } else if (question === 'Você tem tido pensamentos ou comportamentos suicidas?') {
+      if (response === 'Sim' && positiveResponses >= 4) {
+        setQuestion('Identifiquei pelas suas respostas que você corre o risco de problemas psicológicos. Gostaria de Ajuda ?');
+      } else if (response === 'Não' && positiveResponses >= 4) {
+        setQuestion('Identifiquei pelas suas respostas que você corre o risco de problemas psicológicos. Gostaria de Ajuda ?');
+      } else {
+        setQuestion('Fico feliz que está bem! Obrigado por conversar comigo!');
+      }
+    } else if (question === 'Identifiquei pelas suas respostas que você corre o risco de problemas psicológicos. Gostaria de Ajuda ?') {
+      if (response === 'Sim') {
+        setQuestion('Muito bem, entraremos em contato!');
+      } else if (response === 'Não') {
+        setQuestion('Tudo bem, talvez outra hora!');
+      }
+    }
+    setAnswer(response);
+  }
+
+  return (
+    <PaperProvider theme={DefaultTheme}>
+      <Surface style={{ flex: 1, backgroundColor: '#121212' }}>
+        <Appbar.Header style={{ backgroundColor: '#121212' }}>
+          <Appbar.Content title="Bate-Papo" />
+        </Appbar.Header>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Question question={question} answer={answer} />
+          {(question !== 'Muito bem, entraremos em contato!' && question !== 'Tudo bem, talvez outra hora!' && question !== 'Fico feliz que está bem! Obrigado por conversar comigo!') ? (
+            <View style={{ flexDirection: 'row', marginTop: 20 }}>
+              <Button mode="contained" onPress={() => handleAnswer('Sim')} style={{ marginRight: 10 }}>Sim</Button>
+              <Button mode="contained" onPress={() => handleAnswer('Não')}>Não</Button>
+            </View>
+          ) : null}
+        </View>
+      </Surface>
+    </PaperProvider>
   );
 }
